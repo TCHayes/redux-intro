278716cadf02922e43a38d78e2cbfcc74bf44ea7
Terry Hayes <terrychayes@gmail.com>
2017-03-13 12:26:55 -0400

    add store.js and use store.dispatch actions in js/index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,10 @@
 require('babel-polyfill');
 
+import * as actions from './actions/index';
+import store from './store';
+
+store.dispatch(actions.newGame());
+console.log(store.getState());
+
+store.dispatch(actions.addGuess(15));
+console.log(store.getState());
--- js/reducers/index.js
+++ js/reducers/index.js
@@ -4,17 +4,10 @@ const initialGameState = {};
 
 export const gameReducer = (state=initialGameState, action) => {
   if (action.type === actions.NEW_GAME) {
-    return {...state, {
-        number: action.number
-    }}
+    return {...state, number: action.number};
   }
   else if (action.type === actions.ADD_GUESS) {
-    return {...state, {
-      guess: action.guess
-    }}
+    return {...state, guess: action.guess};
   }
   return state;
 }
-
-
-gameReducer(state, newGame())
